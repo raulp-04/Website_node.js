4a6beec1f31325febfc9e698081400125cd81a81
Parasca Raul <raul.parasca@student.tuiasi.ro>
2025-05-04 16:53:36 +0300

    Lab-10 Tema-2, am adaugat /rezultat-chestionar in care apar care raspunsuri sunt gresite si care sunt corecte

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 const port = 6789;
 // directorul 'views' va conține fișierele .ejs (html + js executat la server)
@@ -19,51 +20,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
 app.get('/', (req, res) => res.send('Hello World'));
 
+
+const rawData = fs.readFileSync('./intrebari.json');
+const listaIntrebari = JSON.parse(rawData);
+const raspCorecte = listaIntrebari.map(item => item.corect)
 // la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
-app.get('/chestionar', (req, res) => {
-    const listaIntrebari = [
-        {
-            intrebare: 'Ce instrument este folosit pentru tăierea lemnului?',
-            variante: ['Ciocan', 'Ferăstrău', 'Șurubelniță', 'Pensulă'],
-            corect: 1
-        },
-        {
-            intrebare: 'Care este scopul unei mașini de găurit?',
-            variante: ['Găurirea pereților', 'Lipirea pieselor', 'Tăierea metalului', 'Șlefuirea suprafețelor'],
-            corect: 0
-        },
-        {
-            intrebare: 'Cum se numește procesul de unire a două piese de lemn folosind un șurub?',
-            variante: ['Lipire', 'Îmbinare', 'Șurubare', 'Vopsire'],
-            corect: 2
-        },
-        {
-            intrebare: 'Ce tip de vopsea este recomandată pentru vopsirea suprafețelor din metal?',
-            variante: ['Vopsea acrilică', 'Vopsea pe bază de apă', 'Vopsea specială pentru metal', 'Vopsea de apă'],
-            corect: 2
-        },
-        {
-            intrebare: 'Ce unelte sunt necesare pentru instalarea unui raft pe perete?',
-            variante: ['Ciocan și șurubelniță', 'Ferăstrău și pistol de lipit', 'Mașină de găurit și nivel de apă', 'Vopsitor și pensulă'],
-            corect: 2
-        },
-        {
-            intrebare: 'Care este utilizarea principală a unui slefuitor orbital?',
-            variante: ['Șlefuirea lemnului', 'Tăierea metalului', 'Găurirea betonului', 'Montarea plăcilor ceramice'],
-            corect: 0
-        },
-        {
-            intrebare: 'Ce tip de adeziv este cel mai potrivit pentru fixarea plăcilor ceramice pe perete?',
-            variante: ['Adeziv pentru lemn', 'Adeziv pentru metal', 'Adeziv pentru ceramică', 'Adeziv universali'],
-            corect: 2
-        }  
-        //..
-    ];
+app.get('/chestionar', (req, res) => {    
     // în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari' care conține vectorul de întrebări
     res.render('chestionar', {intrebari: listaIntrebari});
 });
 app.post('/rezultat-chestionar', (req, res) => {
-    console.log(req.body);
-    res.send("formular: " + JSON.stringify(req.body));
+    const raspuns = req.body;
+    Object.entries(raspuns).forEach(([cheie, valoare], index) => {
+       console.log(`${index}-{${cheie}, ${valoare}}, ${raspCorecte[index]}`); 
+    });
+    res.render('rezultat-chestionar', { layout: 'layout', titlu: 'Rezultate', raspunsuri: raspuns , corect: raspCorecte});
 });
 app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:${port}/`));
