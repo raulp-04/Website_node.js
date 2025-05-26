74cf593f628fd38fa63a504bbfabc43c855c621e
Parasca Raul <raul.parasca@student.tuiasi.ro>
2025-05-26 17:34:00 +0300

    Adaugare de admin si rolul de user, adminul poate adauga produse in baza de date, imbunatatirea securitatii website-ului si finalizarea proiectului.

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ app.use(session({
         maxAge: 30 * 60 * 1000
     }
 }));
-
 // la accesarea din browser adresei http://localhost:6789/ se va returna textul 'Hello World'
 // proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
@@ -42,11 +41,27 @@ const produse = [ // Doar pentru testarea insertului
     ['Pistol de lipit cu silicon', 39.99, 12],
     ['Bandă izolatoare (set 5 buc)', 12.99, 50],
     ['Adeziv de construcții 300 ml', 18.50, 40],
-    ['Nivelă cu bulă 40cm', 29.00, 18],
-  ];
+    ['Nivelă cu bulă 40cm', 29.00, 18]
+];
+
+const ipBlockList = {};
+const triedToLogIn = {};
+const MAX_NUMBER_OF_LOGIN = 5;
+const BLOCK_DURATION_MS = 60 * 1000;
 
 app.use((req, res, next) => {
     res.locals.username = req.session.user || '';
+    res.locals.role = req.session.role || '';
+    const ip = req.ip;
+    if (ipBlockList[ip]) {
+        const timeLeft = ipBlockList[ip] - Date.now();
+        if (timeLeft > 0) {
+            return res.status(403).send(`Acces blocat temporar. Încearcă din nou peste ${Math.ceil(timeLeft / 1000)} secunde.`);
+        } else {
+            // Deblocăm automat după ce a expirat timpul
+            delete ipBlockList[ip];
+        }
+    }
     next();
 });
 
@@ -54,9 +69,6 @@ app.get('/', (req, res) => {
     let db = new sqlite3.Database("./cumparaturi.db" , (err) => {
         if(err) {
             console.log("Error Occurred - " + err.message);
-        }
-        else {
-            console.log(`DataBase 'cumparaturi' Connected`);
         }
     });
 
@@ -84,7 +96,6 @@ app.post('/adaugare_cos', (req, res) =>{
     if(!req.session.cos.includes(id)){
         req.session.cos.push(id);
     }
-    console.log(`Cosul: ${req.session.cos}`);
     res.redirect('/');
 });
 
@@ -131,9 +142,6 @@ app.get('/inserare_bd', (req, res) => {
         if(err) {
             console.log("Error Occurred - " + err.message);
         }
-        else {
-            console.log(`DataBase 'cumparaturi' Connected`);
-        }
     });
 
     produse.forEach(([nume, pret, stoc]) => {
@@ -157,49 +165,72 @@ app.get('/vizualizare-cos', (req, res) => {
         if(err) {
             console.log("Error Occurred - " + err.message);
         }
-        else {
-            console.log(`DataBase 'cumparaturi' Connected`);
-        }
     });
+    if(req.session.user) {
+        db.all("SELECT * FROM produse", [], (err, rows) => {
+            if (err) {
+                console.error("Eroare la interogare:", err.message);
+                res.render('vizualizare-cos', {
+                    produse: rows,
+                    isOk: false,
+                    cos: req.session.cos
+                });
+            } else {
+                var cos_nou = [];
+                var sum = 0;
+                rows.forEach(prod => {
+                    if(req.session.cos.includes(prod.id)){
+                        cos_nou.push(prod);
+                        sum+=prod.pret;
+                    }
+                });
+                res.render('vizualizare-cos', {
+                    isOk: true,
+                    cos: cos_nou,
+                    suma: sum
+                });
+            }
+        });
+    } else {
+        ipBlockList[req.ip] = Date.now() + BLOCK_DURATION_MS;
+        res.send("Nu sunteti conectat, resursa nu va fi disponibila.");
+    }
+    db.close();
+});
 
-    db.all("SELECT * FROM produse", [], (err, rows) => {
-        if (err) {
-            console.error("Eroare la interogare:", err.message);
-            res.render('vizualizare-cos', {
-                produse: rows,
-                isOk: false,
-                cos: req.session.cos
-            });
-        } else {
-            var cos_nou = [];
-            rows.forEach(prod => {
-                if(req.session.cos.includes(prod.id)){
-                    cos_nou.push(prod);
-                }
-            });
-            res.render('vizualizare-cos', {
-                isOk: true,
-                cos: cos_nou
-            });
-        }
-        db.close();
-    });
+app.post('/sterge_cos', (req, res) => {
+    req.session.cos.pop(req.body.id);
+    res.redirect('/vizualizare-cos');
 });
 
 app.post('/verificare-autentificare', (req, res) => {
     fs.readFile('utilizatori.json', (err,data) =>{
         const utilizatori = JSON.parse(data);
-        let {user, pass} = req.body;
+        let {user, role ,pass} = req.body;
         const utilizatorGasit = utilizatori.find(u => u.username === user && u.password === pass);
-        if(utilizatorGasit){
+        let ip = req.ip;
+
+        if(utilizatorGasit){ // daca e gasit, ne conectam folosind un session
             req.session.user = utilizatorGasit.username;
+            req.session.role = utilizatorGasit.role;
             if(!req.session.cos){
                 req.session.cos = [];
             }
-            res.redirect('/');
+            delete triedToLogIn[ip];
+            return res.redirect('/');
+        } 
+        // verificam cate incercari mai are si eventual blocam IP-ul
+        if(!triedToLogIn[ip]) {
+            triedToLogIn[ip] = {count: 0};
+        }
+        triedToLogIn[ip].count++;
+
+        if(triedToLogIn[ip].count >= MAX_NUMBER_OF_LOGIN){
+            ipBlockList[ip] = Date.now() + BLOCK_DURATION_MS;
+            return res.status(403).send("Prea multe încercări de autentificare. Acces blocat temporar.");
         } else {
-            res.cookie("error","Autentificare nereusita!");
-            res.redirect('autentificare');
+            res.cookie("error",`Autentificare nereusita! ${MAX_NUMBER_OF_LOGIN - triedToLogIn[ip].count} incercari ramase.`);
+            return res.redirect('autentificare');
         }
     })
 });
@@ -225,6 +256,58 @@ app.post('/rezultat-chestionar', (req, res) => {
         const raspuns = req.body;
         res.render('rezultat-chestionar', { layout: 'layout', titlu: 'Rezultate', raspunsuri: raspuns , corect: raspCorecte});
     });
+});
+
+app.get('/admin', (req, res) => {
+    if(req.session.role == 'ADMIN'){
+        res.render('admin');
+    } else {
+        ipBlockList[req.ip] = Date.now() + BLOCK_DURATION_MS;
+        res.send("Nu sunteti adminul. Va rog sa parasiti website-ul");
+    }
+});
+
+app.post('/adauga_produs', (req, res) => {
+    let db = new sqlite3.Database("./cumparaturi.db" , (err) => {
+        if(err) {
+            console.log("Error Occurred - " + err.message);
+        }
+    });
+    if(req.session.role == 'ADMIN') {
+        let nume = req.body.nume;
+        let pret = parseFloat(req.body.pret);
+        let stoc = parseInt(req.body.stoc);
+        
+        db.run(
+            `INSERT INTO produse (nume, pret, stoc) VALUES (?, ?, ?)`,
+            [nume, pret, stoc],
+            function(err) {
+                if (err) {
+                    console.error('Eroare la inserare:', err.message);
+                }
+            }
+        );
+        res.redirect('/admin');
+
+    } else {
+        ipBlockList[req.ip] = Date.now() + BLOCK_DURATION_MS;
+        res.send("Nu sunteti adminul. Va rog sa parasiti website-ul");
+    }
+    db.close();
+});
+
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+app.get('/.well-known/appspecific/com.chrome.devtools.json', (req, res) => res.status(204).end());
+
+app.use((req, res, next) => {
+    const ip = req.ip;
+    console.warn(`Acces la resursă inexistentă de la IP: ${ip} - URL: ${req.originalUrl}`);
+
+    // Blochează IP-ul
+    ipBlockList[ip] = Date.now() + BLOCK_DURATION_MS;
+
+    res.status(404).send("Resursa nu există. Accesul a fost blocat temporar.");
+    next();
 });
 
 app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:${port}/`));
